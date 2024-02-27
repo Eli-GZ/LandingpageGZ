450df7cfe7d6b176d518e791177bfd16cbae7427
Elias <elias.d.gonzalez25@gmail.com>
2024-02-27 18:41:06 -0300

    buttons

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 let titulo = document.getElementById('titleEffect')
+let productBtns = document.getElementsByClassName('productBtns')
+let titleBtn = document.getElementById('titleBtn')
+
 let isActive = false
 
 function efectoTitulo(){
  if(!isActive){
-  titulo.style.color="gray";
+  titulo.style.color="gray"; 
+  titleBtn.style.color="gray";
+  for(let i = 0; i < productBtns.length;i++){
+    productBtns[i].style.color="gray";
+  }  
+
   isActive=true;
  }else{
-  titulo.style.removeProperty('color');
+  titulo.style.removeProperty('color'); 
+  titleBtn.style.removeProperty('color');
+  for(let i = 0; i < productBtns.length;i++){
+    productBtns[i].style.removeProperty('color');
+  }  
   isActive=false;
  }
 }
